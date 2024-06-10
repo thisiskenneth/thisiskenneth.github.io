2252de69f223141340092b1119270ddaf86f73fb
Kenneth Cortez <kacortez@espe.edu.ec>
2024-06-10 13:46:04 -0500

    g

--- PARTE1/Examen/Script/hide3.js
+++ PARTE1/Examen/Script/hide3.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $("#ShContentButton").click(function() {
-        $("#demo-content3").load("GridSystem/content3.html", function() {
+        $("#demo-content3").load("./GridSystem/content3.html", function() {
             $("#demo-content3").show(500);
         });
     });
